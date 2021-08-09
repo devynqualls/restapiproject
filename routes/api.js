60e6a8e53f1fed9d1006219bb1c3f5cede01e4bd
devynqualls <58799637+devynqualls@users.noreply.github.com>
2021-08-09 16:09:40 -0400

    GET Request
    Completed GET REQUEST handling

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,35 @@ const router = vertex.router()
 
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const controllers = require('../controllers')
+const Player = require('../models/Player')
+const Team = require('../models/Team')
 
+
+router.get('/:resource', (req, res) => {
+  const resource = req.params.resource
+  const controller = controllers[resource] 
+if (controller == null){
+  res.json({
+    confirmation: 'fail',
+    message: 'invalid Resource'
+  })
+  return
+}
+  controller.get()
+  .then(data => {
+    res.json({
+      confirmation: 'success',
+      data: data
+    })
+  })
+  .catch(err => {
+    res.json({
+      confirmation: 'fail',
+      message: err.message 
+    })
+  })
+})
 const players = [
   {firstName: "John", lastName:"Carrol", position: "Point-guard", age: 20, team: "Lakers"},
   {firstName: "David", lastName:"Bucks", position: "Center", age: 27, team: "Nets"},
@@ -23,6 +51,37 @@ const db = {
   team: teams,
   player: players
 }
+
+router.get('/team', (req, res) => {
+  Team.find(null)
+  .then(data => {
+    res.json({
+      confirmation: 'success',
+      data: data
+    })
+  })
+  .catch(err => {
+    res.json({
+      confirmation: 'fail',
+      message: err.message
+    })
+  })
+})
+router.get('/player', (req, res) => {
+  Team.find(null)
+  .then(data => {
+    res.json({
+      confirmation: 'success',
+      data: data
+    })
+  })
+  .catch(err => {
+    res.json({
+      confirmation: 'fail',
+      message: err.message
+    })
+  })
+})
 
 router.get('/:resource', (req, res) =>{
   // sets resource to desired requested data example noted below: player/team
